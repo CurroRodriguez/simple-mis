a5cb4131ae9c4190cf9c78a58af2febcccf96f85
Isaac Rodriguez <isaac.rodriguez@autodesk.com>
2015-05-28 11:47:54 -0400

    Fixes insertion of path to source in docs configuration.
    Turns out I wasn't passing the variable that contained the path to the source to ``sys.path.insert()``.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,8 +21,8 @@ import sphinx_rtd_theme
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 this_dir = os.path.dirname(__file__)
-lib_dir = os.path.join(this_dir, '..', '..', 'proj', 'python', 'source')
-sys.path.insert(0, os.path.abspath('.'))
+src_dir = os.path.join(this_dir, '..', '..', 'proj', 'python', 'source')
+sys.path.insert(0, os.path.abspath(src_dir))
 
 # -- General configuration ------------------------------------------------
 
