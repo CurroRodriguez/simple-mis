906731c6feb937cee50c6479dfbb08a9378b26dc
Isaac Rodriguez <isaac.rodriguez@autodesk.com>
2015-06-02 15:54:45 -0400

    Restructured Project for Python Only Library
    After much thought, I decided that this project will be Python only and that additional libraries for other languages will be provided in separate projects and repos. This allows me to make the structure of the project python specific and remove the ``proj/python`` structure bringing ``source`` and ``tests`` to the root of the project. Changes to the documentation build have been made to support the new structure. I also consolidated the required packages files into a single ``requirements.txt`` which matches what other python projects are doing.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,7 +21,7 @@ import sphinx_rtd_theme
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 this_dir = os.path.dirname(__file__)
-src_dir = os.path.join(this_dir, '..', '..', 'proj', 'python', 'source')
+src_dir = os.path.join(this_dir, '..', '..', 'source')
 sys.path.insert(0, os.path.abspath(src_dir))
 
 # -- General configuration ------------------------------------------------
