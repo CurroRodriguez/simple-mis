eed4e39a64f3df930485e83f68b690be37857568
Isaac Rodriguez <isaac_rodriguez@live.com>
2015-07-07 18:33:07 -0400

    Added setup.py.

---
+++ setup.py
@@ -0,0 +1,14 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='smis',
+    version='0.0.3',
+    description='Simple MIS Library',
+    long_description='A simple library to access Autodesk InfraWorks 360 Model Information Service.',
+    author='Isaac Rodriguez',
+    classifiers = ['Development Status :: 3 - Alpha', 'Programming Language :: Python :: 2.7'],
+    url='https://github.com/CurroRodriguez/smis-python',
+    packages=find_packages('source/smis'),
+    install_requires=['requests>=2.7', 'requests-oauthlib>=0.5']
+)
